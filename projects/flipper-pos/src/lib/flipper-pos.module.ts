323cea529c48bc83a5c98348b93f138e3ef70c4e
StreamUpBox LtdRichard <richard@yegobox.com>
2019-11-05 16:48:27 +0200

    put in place flipper-pos

---
+++ projects/flipper-pos/src/lib/flipper-pos.module.ts
@@ -0,0 +1,12 @@
+import { NgModule } from '@angular/core';
+import { FlipperPosComponent } from './flipper-pos.component';
+
+
+
+@NgModule({
+  declarations: [FlipperPosComponent],
+  imports: [
+  ],
+  exports: [FlipperPosComponent]
+})
+export class FlipperPosModule { }

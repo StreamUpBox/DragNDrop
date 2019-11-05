ec9a381f886dc85fa9b8e555a39cfb25e772eee7
StreamUpBox LtdRichard <richard@yegobox.com>
2019-11-05 14:02:47 +0200

    #1

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,32 +1,34 @@
 import {BrowserModule} from '@angular/platform-browser';
 import {NgModule} from '@angular/core';
-import {AppComponent} from './app.component';
-import {NDropModule} from 'NDrop';
+// import {AppComponent} from './app.component';
+// import {NDropModule} from 'NDrop';
 import { HttpModule, BrowserXhr } from '@angular/http';
 import { FormsModule } from '@angular/forms';
 import { ChildComponent } from './child/child.component';
 import { LoadersDirective } from './loaders.directive';
 import { HttpClientModule } from '@angular/common/http';
 import { InfiniteScrollModule } from 'ngx-infinite-scroll';
-import { NUploadModule } from '../../projects/nupload/src/lib/nupload.module';
+import { DemoComponent } from './demo/demo.component';
+// import { NUploadModule } from '../../projects/nupload/src/lib/nupload.module';
 @NgModule({
     declarations: [
-        AppComponent,
+        // AppComponent,
         ChildComponent,
-        LoadersDirective
+        LoadersDirective,
+        DemoComponent
     ],
     imports: [
         HttpClientModule,
         BrowserModule,
-        NDropModule,
+        // NDropModule,
         HttpModule,
         FormsModule,
         InfiniteScrollModule,
-        NUploadModule
+        // NUploadModule
     ],
     providers: [],
 
-    bootstrap: [AppComponent]
+    bootstrap: [DemoComponent]
 })
 export class AppModule {
 }

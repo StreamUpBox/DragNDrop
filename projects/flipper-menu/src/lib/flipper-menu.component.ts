02ede83629ad98ac9f11d0ddb1ea6f3e0b7199b6
Richard Mura <richard@yegobox.com>
2019-12-05 11:25:31 +0200

    formating

--- projects/flipper-menu/src/lib/flipper-menu.component.ts
+++ projects/flipper-menu/src/lib/flipper-menu.component.ts
@@ -12,9 +12,9 @@ import { Menu } from './menu';
   styleUrls: ['./flipper-menu.component.css'],
   animations: [
     trigger('toggleBox', [
-      
+
       transition('open => closed', [
-       animate('1500ms')
+        animate('1500ms')
       ]),
       transition('closed => open', [
         animate('1000ms')
@@ -23,112 +23,112 @@ import { Menu } from './menu';
   ]
 })
 export class FlipperMenuComponent implements OnInit {
- 
-  @Output() menuToggled:any = new EventEmitter<boolean>();
-  @Output() switchedBusiness:any = new EventEmitter<Business>();
-  @Output() switchedBranch:any = new EventEmitter<Branch>();
-  @Output() routerClicked:any = new EventEmitter<any>();
 
-  @Input() menuEntries:MenuEntries;
+  @Output() menuToggled: any = new EventEmitter<boolean>();
+  @Output() switchedBusiness: any = new EventEmitter<Business>();
+  @Output() switchedBranch: any = new EventEmitter<Branch>();
+  @Output() routerClicked: any = new EventEmitter<any>();
 
-  isOpen:boolean=false;
-  canViewBranches:boolean=false;
-  defaultBranch:Branch=null;
-  loggedUser:User=null;
-  branches:Branch[]=[];
-  businesses:Business[]=[];
-  menus:Menu[]=[];
-  defaultBusiness:Business=null;
-  settingMenu:Menu=null;
+  @Input() menuEntries: MenuEntries;
+
+  isOpen: boolean = false;
+  canViewBranches: boolean = false;
+  defaultBranch: Branch = null;
+  loggedUser: User = null;
+  branches: Branch[] = [];
+  businesses: Business[] = [];
+  menus: Menu[] = [];
+  defaultBusiness: Business = null;
+  settingMenu: Menu = null;
   constructor() { }
 
   ngOnInit() {
-     this.init();
-  
-  }
-  init(){
-    if(this.menuEntries){
+    this.init();
 
-      this.defaultBranch=this.menuEntries.branches.find(b=>b.active==true);
-      this.defaultBusiness=this.menuEntries.businesses.find(b=>b.active==true);
-      this.settingMenu=this.menuEntries.menu.find(m=>m.route=='settings');
-  
-      this.loggedUser=this.menuEntries.user?this.menuEntries.user:null;
-      this.branches = this.menuEntries.branches.length > 0?this.menuEntries.branches:[];
-      this.businesses = this.menuEntries.businesses.length > 0?this.menuEntries.businesses.filter(b=>b.active==false):[];
-      this.menus = this.menuEntries.menu.length > 0?this.menuEntries.menu.filter(m=>m.route!='settings'):[];
-      this.canViewBranches=false;
+  }
+  init() {
+    if (this.menuEntries) {
+
+      this.defaultBranch = this.menuEntries.branches.find(b => b.active == true);
+      this.defaultBusiness = this.menuEntries.businesses.find(b => b.active == true);
+      this.settingMenu = this.menuEntries.menu.find(m => m.route == 'settings');
+
+      this.loggedUser = this.menuEntries.user ? this.menuEntries.user : null;
+      this.branches = this.menuEntries.branches.length > 0 ? this.menuEntries.branches : [];
+      this.businesses = this.menuEntries.businesses.length > 0 ? this.menuEntries.businesses.filter(b => b.active == false) : [];
+      this.menus = this.menuEntries.menu.length > 0 ? this.menuEntries.menu.filter(m => m.route != 'settings') : [];
+      this.canViewBranches = false;
     }
   }
 
-  toggle():boolean{
-     this.isOpen=!this.isOpen;
-     this.menuToggled.emit(this.isOpen);
-     return this.isOpen;
+  toggle(): boolean {
+    this.isOpen = !this.isOpen;
+    this.menuToggled.emit(this.isOpen);
+    return this.isOpen;
   }
-    toggleBranches():boolean{
-        this.canViewBranches=!this.canViewBranches;
-        return this.canViewBranches;
+  toggleBranches(): boolean {
+    this.canViewBranches = !this.canViewBranches;
+    return this.canViewBranches;
+  }
+
+  switchBusiness(business: Business) {
+    const current = this.defaultBusiness;
+    current.active = false;
+    business.active = true;
+
+    this.defaultBusiness = null;
+
+    var businesses: Business[] = this.businesses;
+
+    businesses = businesses.filter(b => b.id !== business.id);
+    if (!businesses.find(b => b.id == current.id)) {
+      businesses.push(current);
     }
 
-    switchBusiness(business:Business){
-     const current=this.defaultBusiness;
-     current.active=false;
-     business.active=true;
+    this.businesses = businesses;
+    this.defaultBusiness = business;
+    this.switchedBusiness.emit(this.defaultBusiness);
+    this.canViewBranches = false;
+  }
 
-     this.defaultBusiness=null;
+  switchBranch(branch: Branch) {
 
-     var businesses:Business[]=this.businesses;
-    
-     businesses=businesses.filter(b=>b.id!==business.id);
-      if(!businesses.find(b=>b.id==current.id)){
-        businesses.push(current);
-      }
-      
-     this.businesses=businesses;
-     this.defaultBusiness=business;
-     this.switchedBusiness.emit(this.defaultBusiness);
-     this.canViewBranches=false;
+    const current = this.defaultBranch;
+    current.active = false;
+    branch.active = true;
+
+    this.defaultBranch = null;
+
+    var branches: Branch[] = this.branches;
+
+    branches = branches.filter(b => b.id !== branch.id);
+    if (!branches.find(b => b.id == current.id)) {
+      branches.push(current);
     }
- 
-    switchBranch(branch:Branch){
-     
-      const current=this.defaultBranch;
-     current.active=false;
-     branch.active=true;
 
-     this.defaultBranch=null;
+    this.branches = branches;
+    this.defaultBranch = branch;
+    this.switchedBranch.emit(this.defaultBranch);
+    this.canViewBranches = false;
 
-     var branches:Branch[]=this.branches;
-    
-     branches=branches.filter(b=>b.id!==branch.id);
-      if(!branches.find(b=>b.id==current.id)){
-        branches.push(current);
-      }
-      
-     this.branches=branches;
-     this.defaultBranch=branch;
-     this.switchedBranch.emit(this.defaultBranch);
-     this.canViewBranches=false;
+  }
+  router(menu: Menu) {
+    if (this.settingMenu.active == true) {
+      this.settingMenu.active = false;
+    } else {
+      this.menus.find(m => m.active == true).active = false;
+    }
 
-     }
-     router(menu:Menu){
-       if(this.settingMenu.active==true){
-        this.settingMenu.active=false;
-       }else{
-        this.menus.find(m=>m.active==true).active=false;
-       }
-     
-      menu.active=true;
-      const menus=this.menus;
-      if(!menus.find(m=>m.id==this.settingMenu.id)){
-        menus.push(this.settingMenu);
-      }
-      this.init();
-      
-      this.routerClicked.emit({menus:menus,router:menu.route});
-     }
-     hideBranchDropDown(){
-       this.canViewBranches=false;
-     }
+    menu.active = true;
+    const menus = this.menus;
+    if (!menus.find(m => m.id == this.settingMenu.id)) {
+      menus.push(this.settingMenu);
+    }
+    this.init();
+
+    this.routerClicked.emit({ menus: menus, router: menu.route });
+  }
+  hideBranchDropDown() {
+    this.canViewBranches = false;
+  }
 }

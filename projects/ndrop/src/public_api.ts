42779329be4cd0f5498c9d1662890d5e3915a1f7
Richard Mura <richard@yegobox.com>
2019-12-05 11:56:02 +0200

    remove the module we won't use for now

--- projects/ndrop/src/public_api.ts
+++
@@ -1,7 +0,0 @@
-/*
- * Public API Surface of ndrop
- */
-
-export * from './lib/ndrop.service';
-export * from './lib/ndrop.component';
-export * from './lib/ndrop.module';
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,8 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { FlipperMenuModule } from 'flipper-menu';
 import { AppRoutingModule } from './app-routing.module';
+import { TestingComponentComponent } from './testing-component/testing-component.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 
 @NgModule({
@@ -13,6 +15,7 @@ import { AppRoutingModule } from './app-routing.module';
   imports: [
     BrowserModule,
     FlipperMenuModule,
+    BrowserAnimationsModule,
     AppRoutingModule
   ],
   providers: [],

06106074b68ed52961ce1b8d6da144d67f308112
Richard Muragijimana <beastar457@gmail.com>
2020-08-14 12:54:31 +0200

    fix hot reload in all projects

--- projects/yegobox-login/src/public-api.ts
+++
@@ -1,7 +0,0 @@
-/*
- * Public API Surface of yegobox-login
- */
-
-export * from './lib/yegobox-login.service';
-export * from './lib/yegobox-login.component';
-export * from './lib/yegobox-login.module';

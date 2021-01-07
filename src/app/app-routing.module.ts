8fdce39fedf1f771f0b9ca75a19db9adb8daa420
Ganza respice <respinho2014@gmail.com>
2021-01-07 20:14:50 +0200

    inventory lib is ready to test

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,11 +2,10 @@ import { NgModule } from '@angular/core'
 import { Routes, RouterModule } from '@angular/router'
 import { ProductsComponent } from '@enexus/flipper-inventory'
 import { CreateProductComponent } from '@enexus/flipper-inventory'
-import { CreateUpdateBusinessComponent } from '@enexus/flipper-settings'
 
 const routes: Routes = [
-  { path: 'index', component: ProductsComponent },
-  { path: '', redirectTo: '/index', pathMatch: 'full' },
+  { path: '', component: ProductsComponent },
+  { path: '', redirectTo: '/', pathMatch: 'full' },
 
   { path: 'admin/inventory', component: ProductsComponent },
   {
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,10 +6,18 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations'
 import { VendorsModule } from '@enexus/flipper-vendors'
 import { FlipperComponentsModule, PouchDBService } from '@enexus/flipper-components'
 import { FlipperInventoryModule } from '@enexus/flipper-inventory'
+import { AppRoutingModule } from './app-routing.module'
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, VendorsModule, FlipperComponentsModule, FlipperInventoryModule, BrowserAnimationsModule],
+  imports: [
+    BrowserModule,
+    VendorsModule,
+    AppRoutingModule,
+    FlipperComponentsModule,
+    FlipperInventoryModule,
+    BrowserAnimationsModule,
+  ],
   entryComponents: [],
   providers: [PouchDBService],
   bootstrap: [AppComponent],

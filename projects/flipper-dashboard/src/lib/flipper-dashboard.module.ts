14d36466e7b954ac3d6e00965e69f193c0eab749
Richard Muragijimana <beastar457@gmail.com>
2021-01-22 19:38:09 +0200

    fixes

--- projects/flipper-dashboard/src/lib/flipper-dashboard.module.ts
+++ projects/flipper-dashboard/src/lib/flipper-dashboard.module.ts
@@ -2,7 +2,6 @@ import { NgModule } from '@angular/core'
 import { FlipperDashboardComponent } from './flipper-dashboard.component'
 import { CommonModule } from '@angular/common'
 import { FlipperComponentsModule } from '@enexus/flipper-components'
-
 @NgModule({
   declarations: [FlipperDashboardComponent],
   imports: [CommonModule, FlipperComponentsModule],
